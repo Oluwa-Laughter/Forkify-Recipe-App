8b5053576395495fb062247d6aa3c241e115a467
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-05-31 17:30:06 +0100

    update pagination and number of servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from "./model.js";
 import recipeview from "./views/recipeview.js";
 import searchView from "./views/searchView.js";
 import resultView from "./views/resultView.js";
+import paginationView from "./views/paginationView.js";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
@@ -36,14 +37,37 @@ const controlSearchResults = async function () {
     // 2) Load search Results
     await model.loadSearchResults(query);
     // 3) Render Results
-    resultView.render(model.state.search.results);
+    // resultView.render(model.state.search.results);
+    resultView.render(model.getSearchResultsPage());
+
+    // 4) Render Initial pagination button
+    paginationView.render(model.state.search);
   } catch (error) {
     console.error(error);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render NEW Results
+  // resultView.render(model.state.search.results);
+  resultView.render(model.getSearchResultsPage(goToPage));
+
+  // 4) Render NEW pagination button
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the Recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeview.render(model.state.recipe);
+};
+
 const init = function () {
   recipeview.addHandlerRender(controlRecipes);
+  recipeview.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
